6f96341cbac7e2f7999770211bbca384761f3f82
mindawei <3092527496.qq.com>
2017-03-20 16:58:01 +0800

    hard问题最后优化一下

--- src/com/cacheserverdeploy/deploy/Global.java
+++ src/com/cacheserverdeploy/deploy/Global.java
@@ -2,9 +2,11 @@ package com.cacheserverdeploy.deploy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 全局参数，方便访问
@@ -15,7 +17,7 @@ import java.util.List;
 public final class Global {
 
 	/** 是否是调试 */
-	static final boolean IS_DEBUG = true;
+	static final boolean IS_DEBUG = false;
 
 	/** 何时超时 */
 	static final long TIME_OUT = System.currentTimeMillis() + 80 * 1000L;
@@ -330,49 +332,34 @@ public final class Global {
 	/** 对接进行优化 */
 	public static void optimize() {
 		
-		System.out.println("Global optimize TODO");
-//		
-//		// 检查每个边出去的费用和在自己的费用
-//		Map<Integer,Integer> costs = new HashMap<Integer,Integer>();
-//		
-//		for(int consumerId =0;consumerId<consumerNum;++consumerId){
-//			costs.put(consumerId, 0);
-//		}
-//		
-//		for(Server server :bestServers){
-//			int serverNode = server.node;
-//			for(int consumerId =0;consumerId<consumerNum;++consumerId){
-//				if(consumerNodes[consumerId]==serverNode){
-//					costs.put(consumerId, depolyCostPerServer);
-//					break;
-//				}
-//			}		
-//		}
-//		
-//		
-//		for(Server server : bestServers){
-//			for(ServerInfo info : server.serverInfos){
-//				int cost = costs.get(info.consumerId);
-//				cost+=info.getBandWidthCost();
-//				costs.put(info.consumerId, cost);
-//			}
-//		}
-//		
-//		int total = 0;
-//		List<Integer> needRemovedConsumeIds = new ArrayList<Integer>();
-//		for(Map.Entry<Integer,Integer> entry : costs.entrySet()){
-//			
-//			System.out.println(entry.getKey()+" : "+entry.getValue());
-//			total+=entry.getValue();
-//			if(entry.getValue()>depolyCostPerServer){
-//				needRemovedConsumeIds.add(entry.getKey());
-//				
-//			}
-//		}
-//		System.out.println("total:"+total);
-//		System.out.println("depolyCostPerServer:"+depolyCostPerServer);
-//		
+		if(!isNpHard){
+			return;
+		}
+
+		Map<Integer, Server> newServers = new HashMap<Integer, Server>();
+		for (Server server : bestServers) {
+			newServers.put(server.node, new Server(server.node));
+		}
+					
+		Global.resetEdgeBandWidth();
 	
+		Server[] consumerServers = Global.getConsumerServer();
+		
+		RouterComplex.transfer(consumerServers, newServers);
+		
+		ArrayList<Server> nextGlobalServers = new ArrayList<Server>();
+		for(Server consumerServer : consumerServers){
+			if (consumerServer.getDemand() > 0) { // 真正安装
+				nextGlobalServers.add(consumerServer);
+			}
+		}
+		
+		for(Server newServer : newServers.values()){
+			if (newServer.getDemand() > 0) { // 真正安装
+				nextGlobalServers.add(newServer);
+			}
+		}
+		updateSolution(nextGlobalServers);
 	}
 	
 	/** 
--- src/com/util/FilePath.java
+++ src/com/util/FilePath.java
@@ -7,8 +7,8 @@ public class FilePath {
 //	public static final String graphFilePath = "case_example/case2.txt";
 //	public static final String graphFilePath = "case_example/case3.txt";
 //	public static final String graphFilePath = "case_example/case4.txt";
-//	public static final String graphFilePath = "case_example/case5.txt";
-	public static final String graphFilePath = "case_example/官网案例.txt";
+	public static final String graphFilePath = "case_example/case5.txt";
+//	public static final String graphFilePath = "case_example/官网案例.txt";
 	public static final String resultFilePath = "result/0.txt";
 	
 	// 官网案例       :  787 / 809
@@ -17,7 +17,7 @@ public class FilePath {
 	// case 2 : 1692 / 1722
 	// case 3 : 2309
 	// case 4 : 1997 
-    // case 5 : 13423
+    // case 5 : 13387
 	// use:16319
 	
 	
